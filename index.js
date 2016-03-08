63ebae0f22a5a5ff505ff9418e4244ec7c9bb782
Ainul Yaqin <ainul.top@gmail.com>
2016-03-08 14:20:56 +0700

    ainul mantab

--- index.js
+++ index.js
@@ -22,6 +22,13 @@ app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x
 
 app.use(cors());
 
+app.use(function (req, res, next) {
+    res.header('Cache-Control', 'private, no-cache, no-store, must-revalidate');
+    res.header('Expires', '-1');
+    res.header('Pragma', 'no-cache');
+    next()
+});
+
 app.use('/logs', express.static('logs'));
 
 /*untuk server trx*/
